8fa44067a9dea4cdd3c425c704725da57efa69fc
elvistjw <elvistjw1@gmail.com>
2020-04-25 18:45:38 -0300

    Validação dos dados de entrada do usuário

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,3 +1,5 @@
+import * as Yup from 'yup';
+
 import jwt from 'jsonwebtoken';
 
 import User from '../models/User';
@@ -5,6 +7,15 @@ import authConfig from '../../config/auth';
 
 class SessionController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            nr_cpf: Yup.string().required(),
+            senha: Yup.string().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(401).json({ error: 'Validation fails' });
+        }
+
         const { nr_cpf, senha } = req.body;
 
         const user = await User.findOne({ where: { nr_cpf } });
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,21 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            nm_usuario: Yup.string().required(),
+            ds_email: Yup.string().email().required(),
+            nr_cpf: Yup.string().required(),
+            dt_nascimento: Yup.string().required(),
+            senha: Yup.string().required().min(6),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(401).json({ error: 'Validation fails' });
+        }
+
         const userExists = await User.findOne({
             where: { nr_cpf: req.body.nr_cpf },
         });
@@ -30,11 +44,32 @@ class UserController {
     }
 
     async update(req, res) {
+        const schema = Yup.object().shape({
+            nm_usuario: Yup.string(),
+            ds_email: Yup.string().email(),
+            nr_cpf: Yup.string(),
+            dt_nascimento: Yup.string(),
+            senha_antiga: Yup.string().min(6),
+            senha: Yup.string()
+                .min(6)
+                .when('senha_antiga', (senha_antiga, field) =>
+                    senha_antiga ? field.required() : field
+                ),
+            confirmPassword: Yup.string().when('senha', (senha, field) =>
+                senha ? field.required().oneOf([Yup.ref('senha')]) : field
+            ),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(401).json({ error: 'Validation fails' });
+        }
+
         const { nr_cpf, senha_antiga } = req.body;
 
         const user = await User.findByPk(req.userId);
 
         if (nr_cpf !== user.nr_cpf) {
+            // eslint-disable-next-line no-shadow
             const userExists = await User.findOne({
                 where: { nr_cpf },
             });
