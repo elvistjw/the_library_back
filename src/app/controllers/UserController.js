3d53eec15c1e281ec9e5749349a5edab94e1ba66
elvistjw <elvistjw1@gmail.com>
2020-04-25 18:07:49 -0300

    Edição de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -30,9 +30,39 @@ class UserController {
     }
 
     async update(req, res) {
-        console.log(req.userId);
+        const { nr_cpf, senha_antiga } = req.body;
 
-        return res.json({ ok: true });
+        const user = await User.findByPk(req.userId);
+
+        if (nr_cpf !== user.nr_cpf) {
+            const userExists = await User.findOne({
+                where: { nr_cpf },
+            });
+
+            if (userExists) {
+                return res.status(400).json({ error: 'User already exists.' });
+            }
+        }
+
+        if (senha_antiga && !(await user.checkPassword(senha_antiga))) {
+            return res.status(401).json({ error: 'Password does not match.' });
+        }
+
+        const {
+            id,
+            nm_usuario,
+            ds_email,
+            dt_nascimento,
+            nr_telefone,
+        } = await user.update(req.body);
+
+        return res.json({
+            id,
+            nm_usuario,
+            ds_email,
+            dt_nascimento,
+            nr_telefone,
+        });
     }
 }
 
